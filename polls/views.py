16731b897701f4d39b0054b92e28914c7f6d0114
liaozeyu <1758521090@qq.com>
2024-04-12 14:54:40 +0000

    django_part3

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
-from django.http import HttpResponse
+# render is a faster function
+# from django.http import HttpResponse
+# from django.template import loader
 
+from .models import Question
 
+# from django.http import Http404 # 404 
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    # http function
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template("polls/index.html")
+    # context = {
+    #     "latest_question_list": latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
