c0498ad8dff6ba6265614a3b15679b29612f76eb
liaozeyu <1758521090@qq.com>
2024-04-13 03:28:36 +0000

    basic_test and models

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice,Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+# class ChoiceInline(admin.TabularInline): ...
+
+class QuestionAdmin(admin.ModelAdmin):
+    '''
+    后台配置
+    '''
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
