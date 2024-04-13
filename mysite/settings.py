cf0d790cef748418587ad327b47ddfbfb71e9ce8
liaozeyu <1758521090@qq.com>
2024-04-13 07:50:09 +0000

    demo_end

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # "django_polls.apps.PollsConfig", # polls
+    
+    "debug_toolbar", # debug
     "polls.apps.PollsConfig", # polls
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
